34f8c2e827f177984732c466c51bf5dab52d97f9
GayanthaSandeep16 <gayantha.20210118@iit.ac.lk>
2025-09-19 19:47:32 +0530

    feat:add InventoryController and InventoryService for inventory management

---
+++ src/main/java/com/example/fidenz/service/InventoryService.java
@@ -0,0 +1,48 @@
+package com.example.fidenz.service;
+
+import com.example.fidenz.entity.Inventory;
+import com.example.fidenz.entity.Store;
+import com.example.fidenz.repository.InventoryRepository;
+import com.example.fidenz.repository.StoreRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * This service handles inventory-related operations such as
+ * fetching and updating inventory data.
+ *
+ */
+
+@Service
+public class InventoryService {
+
+
+    private final InventoryRepository inventoryRepository;
+    private final StoreRepository storeRepository;
+
+    public InventoryService(InventoryRepository inventoryRepository, StoreRepository storeRepository) {
+        this.inventoryRepository = inventoryRepository;
+        this.storeRepository = storeRepository;
+    }
+
+    public List<Inventory> getInventoryByStore(Long storeId) {
+        Store store = storeRepository.findById(storeId)
+                .orElseThrow(() -> new RuntimeException("Store not found"));
+        return inventoryRepository.findByStore(store);
+    }
+
+    public Inventory updateInventory(Long inventoryId, Integer newStock) {
+        Inventory inventory = inventoryRepository.findById(inventoryId)
+                .orElseThrow(() -> new RuntimeException("Inventory not found"));
+        
+        inventory.setCurrentStock(newStock);
+        return inventoryRepository.save(inventory);
+    }
+
+    public List<Inventory> getAllInventories() {
+        return inventoryRepository.findAll();
+    }
+}
